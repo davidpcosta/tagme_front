14c22d7cbaebd1e7957fd51ee01b719f7a3b48ef
David Costa <davidpcosta@gmail.com>
2020-02-11 20:18:49 -0300

    componente item receita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ReceitasComponent } from './pages/receitas/receitas.component';
+import { ItemReceitaComponent } from './components/item-receita/item-receita.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ReceitasComponent
+    ReceitasComponent,
+    ItemReceitaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/item-receita/item-receita.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-receita',
+  templateUrl: './item-receita.component.html',
+  styleUrls: ['./item-receita.component.scss']
+})
+export class ItemReceitaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
