578b20c8d84eb1d2da19f06970a0de8d8473f27f
David Costa <davidpcosta@gmail.com>
2020-02-11 22:41:31 -0300

    detalhe receita service

--- src/app/pages/detalhe-receita/detalhe-receita.component.ts
+++ src/app/pages/detalhe-receita/detalhe-receita.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ReceitasService } from 'src/app/services/receitas.service';
+import { ActivatedRoute } from '@angular/router';
+import { DetalheReceita } from 'src/app/services/interface/detalhe-receita';
 
 @Component({
   selector: 'app-detalhe-receita',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalheReceitaComponent implements OnInit {
 
-  constructor() { }
+  private _receitaId = this.route.snapshot.paramMap.get("id");
+  public receita: DetalheReceita = {};
+
+  constructor(private service: ReceitasService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.service.getDetalheReceita(this._receitaId).subscribe(
+      data => {
+        this.receita = data;
+        console.log(data);
+        
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
 }
