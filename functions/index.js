6aaee3f097f65dc1b8209ac97ad47a607aa5a9f3
David Costa <davidpcosta@gmail.com>
2020-02-12 09:14:37 -0300

    node login service

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,40 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const crypto = require('crypto')
 const fs = require('fs');
 
 const app = express();
 
-app.get('/receitas', (request, response) => {
-    let rawdata = fs.readFileSync('data/receitas.json');
-    let receitas = JSON.parse(rawdata);
-    response.send(receitas);
+app.post('/login', (request, response) => {
+    try {
+        const { login, password } = request.body;
+        console.log(login);
+        
+        if (login != "user" || password != "1234") {
+            return response.status(401).send({error: 'Login falhou! Cheque suas credenciais.'});
+        }
+        
+        response.send({
+            "success": true,
+            "request_token": crypto.randomBytes(20).toString('hex'),
+            "expires_at": new Date(Date.now() + (30 * 86400 * 1000))
+        });
+        
+    } catch (error) {
+        response.status(400).send(error);
+    }
 });
 
-app.get('/receita', (request, response) => {
-    let rawdata = fs.readFileSync('data/receitas.json');
-    let receitas = JSON.parse(rawdata);
-    response.send(receitas[2]);
-});
+// app.get('/receitas', (request, response) => {
+//     let rawdata = fs.readFileSync('data/receitas.json');
+//     let receitas = JSON.parse(rawdata);
+//     response.send(receitas);
+// });
+
+// app.get('/receita', (request, response) => {
+//     let rawdata = fs.readFileSync('data/receitas.json');
+//     let receitas = JSON.parse(rawdata);
+//     response.send(receitas[2]);
+// });
 
 exports.api = functions.https.onRequest(app);
