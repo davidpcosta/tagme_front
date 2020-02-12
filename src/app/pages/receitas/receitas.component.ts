4cf779ea4049a6f42f1f875beda435952fd93462
David Costa <davidpcosta@gmail.com>
2020-02-11 22:10:02 -0300

    filtrando receitas

--- src/app/pages/receitas/receitas.component.ts
+++ src/app/pages/receitas/receitas.component.ts
@@ -8,21 +8,29 @@ import { ReceitasService } from 'src/app/service/receitas.service';
 })
 export class ReceitasComponent implements OnInit {
 
-  public receitas = [];
+  private _receitas = [];
+  public searchText = "";
 
   constructor(private service: ReceitasService) { }
 
   ngOnInit(): void {
     this.service.getReceitas().subscribe(
       data => {
-        this.receitas = data;
+        this._receitas = data;
       },
       error => {
         console.log(error);
-        
       }
-      
     );
+  }
+
+  receitas() {
+    if (this.searchText) {
+      return this._receitas.filter(
+        item => item.title.toLowerCase().indexOf(this.searchText.toLowerCase()) > -1
+      );
+    }    
+    return this._receitas;
   }
 
 }
